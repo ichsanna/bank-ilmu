4bdbe29fc2e6f78d25f2a71b1bbfac34c548eb5b
arifka-rizki <rifka.rizki.fakhriza@gmail.com>
2020-12-13 13:37:50 +0700

    Add navbar and home page

--- Bank Ilmu/Controllers/HomeController.cs
+++ Bank Ilmu/Controllers/HomeController.cs
@@ -15,15 +15,11 @@ namespace Bank_Ilmu.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
